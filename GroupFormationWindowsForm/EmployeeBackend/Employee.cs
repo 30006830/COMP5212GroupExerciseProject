603d12c0e6e39c12013449327615bc4f6583511c
Jac Rockell <30013256@student.toiohomai.ac.nz>
2019-09-04 15:03:29 +1200

    BackEndCommit1

---
+++ GroupFormationWindowsForm/EmployeeBackend/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeBackend
+{
+    public class Employee
+    {
+        public string Bank { get; set; }
+        public int IRD { get; set; }
+        public double Pay { get; set; }
+        public double Hrs { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
